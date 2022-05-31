4811e7d55324976b079aac824537b346763cd04a
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-05-31 20:59:46 +0300

    add;) header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
- const drropdownsNode = document.querySelectorAll('.header-nav-drropdown');
+ const drropdownsNode = document.querySelectorAll('.nav__items-drropdown');
  drropdownsNode.forEach(function(event) {
      event.addEventListener('mouseover', function() {
          event.classList.add('active')
      })
-
-
+     event.addEventListener('mouseout', function() {
+         event.classList.remove('active')
+     })
  })
